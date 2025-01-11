065fb43ca6ed91a52bf750fd9b59e18c6ab1daf7
rttf <74861784+RTF47@users.noreply.github.com>
2025-01-11 14:17:58 +0000

    Обновлено Создание и взаимодействие с БД Дописан Промпт Добавлены Новые Кнопки

--- app/handlers/calories_calculator_handler.py
+++ app/handlers/calories_calculator_handler.py
@@ -1,11 +1,11 @@
 import sqlite3
 
-from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 from aiogram import Router
 from app.calories_calculator import calculate_calories
+import app.keyboards as kb
 router = Router()
 
 
@@ -17,82 +17,70 @@ class Calories_calculator(StatesGroup):
     activity = State()
 
 
-@router.message(lambda message: message.text == 'Калькулятор калорий')
+@router.message(lambda message: message.text == 'Параметры тела')
 async def calories_calculator(message: Message, state: FSMContext):
     await state.set_state(Calories_calculator.age)
-    await message.answer('Введите ваш возраст')
+    await message.answer('Введите ваш возраст (цифрой)')
 
 
 @router.message(Calories_calculator.age)
 async def age(message: Message, state: FSMContext):
     await state.update_data(age=int(message.text))
     await state.set_state(Calories_calculator.sex)
-    await message.answer('Введите ваш пол')
+    await message.answer('Выберите ваш пол',reply_markup=kb.user_sex)
 
 
 @router.message(Calories_calculator.sex)
 async def sex(message: Message, state: FSMContext):
     await state.update_data(sex=message.text)
     await state.set_state(Calories_calculator.weight)
-    await message.answer('Введите ваш вес')
+    await message.answer('Введите ваш вес (в килограммах)')
 
 
 @router.message(Calories_calculator.weight)
 async def weight(message: Message, state: FSMContext):
     await state.update_data(weight=int(message.text))
     await state.set_state(Calories_calculator.height)
-    await message.answer('Введите ваш рост')
+    await message.answer('Введите ваш рост (в сантиметрах)')
 
 
 @router.message(Calories_calculator.height)
 async def height(message: Message, state: FSMContext):
     await state.update_data(height=int(message.text))
     await state.set_state(Calories_calculator.activity)
-    await message.answer('Введите ваш уровень активности')
+    await message.answer('Сколько раз в неделю вы занимаетесь спортом?',reply_markup=kb.user_activity)
 
 
 @router.message(Calories_calculator.activity)
 async def activity(message: Message, state: FSMContext):
     await state.update_data(activity=message.text)
     data = await state.get_data()
-    last_dict = generating()
     user_id = str(message.from_user.id)
-    if user_id not in last_dict.keys():
-        connection = sqlite3.connect('tablet.sql')
-        cursor = connection.cursor()
-        calories = calculate_calories(data["age"], data["weight"], data["height"], data["sex"],  data["activity"])
-        sql = f'INSERT INTO users (ides, calories) VALUES ("{user_id}","{calories}")'
-        cursor.execute(sql)
-        connection.commit()
-        await message.answer(f"{calories}")
-        await state.clear()
+    connection = sqlite3.connect('tablet.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT id FROM users WHERE id = ?',(user_id,))
+    anydata = cursor.fetchone()
+    calories = calculate_calories(data["age"], data["weight"], data["height"], data["sex"], data["activity"])
+
+    if anydata:
+        sql = '''
+        UPDATE users 
+        SET calories = ? 
+        WHERE id = ?
+        '''
+        cursor.execute(sql,(calories,user_id))
+        await message.answer(f"Ваша суточная норма калорий для поддержания веса - {calories}.\n"
+                             "Теперь определите свои 'Предпочтения' и получите План питания.",
+                             reply_markup=kb.main_menu)
     else:
-        await message.answer(generating()[user_id])
-        await state.clear()
+        sql = '''
+        INSERT INTO users (id, calories)
+        VALUES (?, ?)
+        '''
+        cursor.execute(sql, (user_id,calories))
+        await message.answer(f'Ваша суточная норма калорий для поддержания веса - {calories}\n'
+                             "Если 'Предпочтения' уже определны, вы можете получить План питания.",reply_markup=kb.main_menu)
+    await state.clear()
+    connection.commit()
     cursor.close()
     connection.close()
-
-
-def generating():
-    connection = sqlite3.connect('tablet.sql')
-    cursor = connection.cursor()
-
-    passes = 'SELECT calories FROM users'
-    cursor.execute(passes)
-    rows1 = cursor.fetchall()
-
-    ids = 'SELECT ides FROM users'
-    cursor.execute(ids)
-    rows2 = cursor.fetchall()
-
-    id_calories = idict(rows1,rows2)
-
-    return id_calories
-
-
-def idict(a,b):
-    c = {}
-    for i,j in zip(b,a):
-        c[i[0]] = j[0]
-
-    return c
--- app/handlers/utility_handlers.py
+++ app/handlers/utility_handlers.py
@@ -2,17 +2,19 @@ from aiogram.filters import CommandStart
 from aiogram import Router
 from aiogram.types import Message
 import app.keyboards as kb
+
+
 router = Router()
 
 
-# Start handler
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Hello!', reply_markup=kb.main_menu)
+    await message.answer(f'Привет, {message.from_user.first_name}!\n'
+                         'Если хочешь получить персональный план питания, тогда выполни небольшую настройку,'
+                          ' нажимая кнопки "Предпочтения" и "Параметры тела".', reply_markup=kb.settings)
+
 
 # Help handler
-
-
 @router.message(lambda message: message.text == 'Помощь')
 async def help(message: Message):
     await message.answer('Список команд:\nКалькулятор калорий\nПомощь\nМои данные')
